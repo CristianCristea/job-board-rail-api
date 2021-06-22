e35be5eb1d02e4f44d5b0a5f79013d453f27be66
Cristian Cristea <cristiancristea.1987@gmail.com>
2021-06-22 15:31:15 +0000

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Job.create(
+  company: 'Amazon',
+  position: 'Staff Software Engineer',
+  description: 'Be the lead technical resource'
+)
+
+Job.create(
+  company: 'Microsoft',
+  position: 'Engineer',
+  description: 'Write code'
+)
